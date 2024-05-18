024dc7ac6808a483c385cf1ac9da9a6eec83dd3a
venkatesh c <venkat@print2block.com>
2024-05-19 03:01:36 +0530

    transparent sign issue fixed

--- src/Pages/Panel/index.svelte
+++ src/Pages/Panel/index.svelte
@@ -1,95 +1,18 @@
 <script>
-  import { onMount } from "svelte"
   import Header from "./components/header.svelte"
   import Upload from "./components/upload.svelte"
   import TopSubNav from "./components/topSubNav.svelte"
   import SignaturePanel from "./shared/signaturePanel.svelte"
 
-  let canvas
-  let ctx
-  let isDrawing = false
-  let signatureDrawn = false
   let showSignPanel = false
   let pdf
   let showPdf = false
+  let signImg
 
-  onMount(() => {
-    try {
-      if (canvas) {
-        ctx = canvas.getContext("2d")
-      }
-    } catch (error) {
-      console.error(error)
-    }
-  })
-
-  const startDrawing = (e) => {
-    isDrawing = true
-    draw(e)
+  $:{
+    console.log(signImg);
   }
 
-  const draw = (e) => {
-    if (!isDrawing) return
-
-    ctx.lineWidth = 2
-    ctx.lineCap = "round"
-    ctx.strokeStyle = "#000"
-
-    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
-
-    signatureDrawn = true 
-  }
-
-  const startDrawingTouch = (e) => {
-    isDrawing = true
-    drawTouch(e.touches[0])
-  }
-
-  const drawTouch = (e) => {
-    if (!isDrawing) return
-
-    ctx.lineWidth = 2.5
-    ctx.lineCap = "round"
-    ctx.strokeStyle = "#000"
-
-    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
-
-    signatureDrawn = true 
-  }
-
-  const stopDrawing = () => {
-    isDrawing = false
-    ctx.beginPath()
-  }
-
-  const clearCanvas = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    signatureDrawn = false // Reset signature drawn
-  }
-
-  const downloadSignature = () => {
-    const url = canvas.toDataURL("image/png")
-    const link = document.createElement("a")
-    link.href = url
-    link.download = "signature.png"
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-  }
-
-  const placeSign = () => {
-    try {
-      console.log(canvas.toDataURL("image/png"))
-    } catch (error) {
-      console.error(error);
-    }
-  }
 </script>
 
 <div class="relative flex min-h-screen w-screen flex-col items-center justify-center gap-3 overflow-hidden bg-gradient-to-r from-gray-700 to-gray-900">
@@ -98,4 +21,4 @@
   <Upload bind:pdf bind:showPdf />
 </div>
 
-<SignaturePanel on:mousedown={startDrawing} on:mousemove={draw} on:mouseup={stopDrawing} on:mouseout={stopDrawing} on:touchstart={startDrawingTouch} on:touchmove={drawTouch} on:touchend={drawTouch} bind:canvas bind:signatureDrawn bind:showSignPanel on:click={clearCanvas} on:downloadSignature={downloadSignature} on:close={() => (showSignPanel = false)} on:ok={placeSign}/>
+<SignaturePanel bind:showSignPanel bind:signImg/>
--- src/Pages/Panel/shared/signaturePanel.svelte
+++ src/Pages/Panel/shared/signaturePanel.svelte
@@ -1,27 +1,136 @@
 <script>
-  import { createEventDispatcher } from "svelte"
+  import { createEventDispatcher, onMount } from "svelte"
   const dispatch = createEventDispatcher()
+  export let signImg
   export let showSignPanel = false
-  export let canvas
-  export let signatureDrawn = false
-  export let showPdf = false
+
+  let canvas
+  let ctx
+  let isDrawing = false
+  let signatureDrawn = false
+  let showPdf = false
+
+  onMount(() => {
+    try {
+      if (canvas) {
+        ctx = canvas.getContext("2d")
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  })
+
+  const startDrawing = (e) => {
+    isDrawing = true
+    draw(e)
+  }
+
+  const draw = (e) => {
+    if (!isDrawing) return
+
+    ctx.lineWidth = 2
+    ctx.lineCap = "round"
+    ctx.strokeStyle = "#000"
+
+    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+    ctx.stroke()
+    ctx.beginPath()
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+
+    signatureDrawn = true // Set signature drawn to true
+  }
+
+  const startDrawingTouch = (e) => {
+    isDrawing = true
+    drawTouch(e.touches[0])
+  }
+
+  const drawTouch = (e) => {
+    if (!isDrawing) return
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    ctx.lineWidth = 2.5
+    ctx.lineCap = "round"
+    ctx.strokeStyle = "#000"
+
+    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+    ctx.stroke()
+    ctx.beginPath()
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+
+    signatureDrawn = true // Set signature drawn to true
+  }
+
+  const stopDrawing = () => {
+    isDrawing = false
+    ctx.beginPath()
+  }
+
+  const clearCanvas = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    signatureDrawn = false // Reset signature drawn
+  }
+
+  const downloadSignature = () => {
+    const url = canvas.toDataURL("image/png")
+    const link = document.createElement("a")
+    link.href = url
+    link.download = "signature.png"
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+  const placeSign = () => {
+    const aspectRatio = 5.5 / 1.2
+
+    // Calculate new dimensions while maintaining aspect ratio
+    let newWidth, newHeight
+    if (canvas.width / canvas.height > aspectRatio) {
+      newWidth = (canvas.height * aspectRatio) / 1.5
+      newHeight = canvas.height
+    } else {
+      newWidth = canvas.width / 1.5
+      newHeight = canvas.width / aspectRatio
+    }
+
+    // Create a temporary canvas to hold the resized image
+    const tempCanvas = document.createElement("canvas")
+    const tempCtx = tempCanvas.getContext("2d")
+
+    // Set the new canvas dimensions
+    tempCanvas.width = newWidth
+    tempCanvas.height = newHeight
+
+    // Set transparent background
+    tempCtx.clearRect(0, 0, tempCanvas.width, tempCanvas.height)
+
+    // Draw the original signature onto the temporary canvas with the new dimensions
+    tempCtx.drawImage(canvas, 0, 0, newWidth, newHeight)
+
+    // Update the signImg with the resized signature with transparent background
+    signImg = tempCanvas.toDataURL("image/png")
+  }
+
+  const closePanel = () => {
+    try {
+      showSignPanel = false
+      clearCanvas()
+    } catch (error) {
+      console.error(error)
+    }
+  }
 </script>
 
-<div class:hidden={showSignPanel && showPdf} class="relative z-10">
+<div class:hidden={!showSignPanel && !showPdf} class="relative z-10">
   <div class="fixed inset-0 z-30 bg-black/60 backdrop-blur-md transition-opacity" />
   <div class="fixed inset-0 z-40 flex w-screen flex-col items-center justify-center gap-3 overflow-y-auto">
-    <div class="flex h-auto w-3/5 flex-col bg-black/60 rounded-md gap-6 border border-dashed border-gray-700 p-6 shadow-2xl">
-      <h1 class="text-left font-semibold text-lg text-orange-500">Draw and Place Signature</h1>
-      <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-      <canvas on:mousedown on:mousemove on:mouseup on:mouseout on:touchstart on:touchmove on:touchend bind:this={canvas} width="400" height="100" class="z-30 cursor-pointer rounded-md border border-gray-500 bg-white" />
-      <div class="flex w-full items-center justify-start gap-3">
-        <button on:click={() => dispatch("close")} type="button" class="flex min-w-fit items-center justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold text-white hover:bg-gray-900 active:bg-gray-800">Close</button>
-        <button disabled={!signatureDrawn} on:click={() => dispatch("ok")} type="button" class="flex min-w-fit items-center justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold text-white hover:bg-green-600 active:bg-green-500 disabled:cursor-not-allowed">Place Signature</button>
-      </div>
-    </div>
-    <div class="flex w-3/5 items-center justify-end gap-3">
-      <button on:click type="button" class="flex min-w-[36px] items-center justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm text-white hover:bg-red-600 active:bg-red-500 disabled:cursor-not-allowed" disabled={!signatureDrawn}>Clear</button>
-      <button on:click={() => dispatch("downloadSignature")} type="button" class="flex min-w-[36px] items-center justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm text-white hover:bg-blue-600 active:bg-blue-500 disabled:cursor-not-allowed" disabled={!signatureDrawn}>Download Signature</button>
+    <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+    <canvas on:mousedown={startDrawing} on:mousemove={draw} on:mouseup={stopDrawing} on:mouseout={stopDrawing} on:touchstart={startDrawingTouch} on:touchmove={drawTouch} on:touchend={drawTouch} bind:this={canvas} width="400" height="200" class="z-30 cursor-pointer rounded-md border border-gray-500 bg-white" />
+    <div class="flex items-center justify-center gap-3">
+      <button on:click={closePanel} type="button" class="flex min-w-fit items-center justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold text-white hover:bg-gray-900 active:bg-gray-800">Close</button>
+      <button on:click={placeSign} type="button" class="flex min-w-fit items-center justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold text-white hover:bg-green-600 active:bg-green-500 disabled:cursor-not-allowed" disabled={!signatureDrawn}>Place Signature</button>
+      <button on:click={clearCanvas} type="button" class="flex min-w-[36px] items-center justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm text-white hover:bg-red-600 active:bg-red-500 disabled:cursor-not-allowed" disabled={!signatureDrawn}>Clear</button>
+      <button on:click={downloadSignature} type="button" class="flex min-w-[36px] items-center justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm text-white hover:bg-blue-600 active:bg-blue-500 disabled:cursor-not-allowed" disabled={!signatureDrawn}>Download Signature</button>
     </div>
   </div>
 </div>
